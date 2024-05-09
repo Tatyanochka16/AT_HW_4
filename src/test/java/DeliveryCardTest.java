d7ab890e106db41125f2076e4dc08a4de1f74799
Tatyanochka <tanyasmit16@yandex.ru>
2024-05-09 15:16:20 +0300

    one test commit

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -31,22 +31,5 @@ public class DeliveryCardTest {
         $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + date));
     }
 
-//    @Test
-//    public void shouldTestWithList() {
-//        open("http://localhost:9999");
-//        SelenideElement form = $(".form");
-//        form.$("[data-test-id='city'] input").setValue("На");
-//        $(".menu").find(withText("Нарьян-Мар")).click();
-//        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.BACK_SPACE);
-//        form.$("[data-test-id='date'] .input__box").click();
-//        LocalDate date = LocalDate.now().plusDays(7);
-//        //??
-//        form.$("[data-test-id='name'] input").setValue("Василий");
-//        form.$("[data-test-id='phone'] input").setValue("+79270000000");
-//        form.$("[data-test-id='agreement']").click();
-//        form.$(".button").click();
-//        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-//        $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на "));
-//    }
 }
 
